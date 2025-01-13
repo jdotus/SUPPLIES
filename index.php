ee52663fb8fd4658d3d8cb3c88db73943b436296
Jay <jaycaigoy@otusinc.com>
2025-01-13 09:09:24 +0800

    index

--- index.php
+++ index.php
@@ -38,6 +38,43 @@ include('dbcon.php');
         </form>
     </div>
 
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Launch static backdrop modal
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Add Supplies</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                .....
+                <form>
+                    <div class="form-group">
+                        <label> Supply</label>
+                        <input type="text" name="supply" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label> Supply</label>
+                        <input type="text" name="supply" class="form-control">
+                    </div>
+                </form>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
+
+
     <div id="scanbarcode" class="result_container"></div>
 
     <script>
@@ -73,7 +110,14 @@ include('dbcon.php');
                 }
             });
         });
+
+
+
+   
     </script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
